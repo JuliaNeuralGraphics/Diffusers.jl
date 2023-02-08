605f7583c05c668ec7a4d4e5865f31fdb75d4b50
Harish Anand <Harish.Anand@amd.com>
2023-02-08 23:25:54 +0000

    add torch.ModuleList

---
+++ src/torch/modulelist.jl
@@ -0,0 +1,35 @@
+
+# torch.ModuleList
+# Actual implementation
+# https://github.com/chengchingwen/Transformers.jl/blob/8de6eb7d3f03b94fb1077588a960803f37f08496/src/huggingface/models/base.jl#L110
+# MIT License
+
+struct ModuleList{N, T<:Tuple}
+    _modules::T
+    ModuleList(ms) = new{length(ms), typeof(ms)}(ms)
+end
+  
+ModuleList(ms...) = ModuleList(ms)
+ModuleList(ms::Vector) = ModuleList(Tuple(ms))
+
+Functors.functor(::Type{<:ModuleList}, modulelist) = modulelist._modules, y -> ModuleList(y)
+
+Base.iterate(modulelist::ModuleList) = iterate(modulelist._modules)
+Base.iterate(modulelist::ModuleList, i...) = iterate(modulelist._modules, i...)
+Base.length(::ModuleList{N}) where N = N
+Base.getindex(modulelist::ModuleList, i) = modulelist._modules[i]
+  
+function get_state_dict(state, prefix, modulelist::ModuleList)
+    param = Functors.functor(modulelist)[1]
+    for (i, layer) in enumerate(modulelist)
+        cprefix = join((prefix, i-1), '.')
+        get_state_dict(state, cprefix, layer)
+    end
+end
+  
+function load_state!(layer::ModuleList, state)
+    for (layerᵢ, stateᵢ) in zip(layer, state)
+        load_state!(layerᵢ, stateᵢ)
+    end
+end
+  
