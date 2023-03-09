edf29ab3854ef7c3734ae1bf24dc3c83948ff510
Harish <harishanand95@gmail.com>
2023-03-08 19:57:55 -0800

    fix: args, downsamplers

--- src/unet_2d.jl
+++ src/unet_2d.jl
@@ -8,12 +8,11 @@ Flux.@functor CrossAttnDownBlock2D
 
 function CrossAttnDownBlock2D(;
         in_channels::Int, out_channels::Int, time_emb_channels::Int,
-        dropout::Real=0, n_layers::Int=1, resnet_time_scale_shift::Bool=false, 
-        resnet_λ=swish, resnet_groups::Integer=32, attn_n_heads=1, down_padding=1, 
-        context_dim=1280, add_downsample=true, use_linear_projection=false)
-    resnets = []
-    attentions = []
-    
+        dropout::Real = 0, n_layers::Int = 1, resnet_time_scale_shift::Bool = false, 
+        resnet_λ = swish, resnet_groups::Int = 32, attn_n_heads::Int = 1, 
+        down_padding::Int = 1, context_dim = 1280, add_downsample::Bool = true, 
+        use_linear_projection::Bool = false)
+
     resnets = Chain([ResnetBlock2D(;in_channels=(i == 1 ? in_channels : out_channels), 
         λ = resnet_λ, out_channels=out_channels, time_emb_channels=time_emb_channels, 
         n_groups=resnet_groups, dropout=dropout, 
@@ -24,11 +23,9 @@ function CrossAttnDownBlock2D(;
         n_norm_groups=resnet_groups, use_linear_projection=use_linear_projection, 
         context_dim=context_dim,) for i in 1:n_layers]...)
 
-    downsamplers = nothing
-    if add_downsample
-        downsamplers = Chain(
-            Conv((3, 3), in_channels => out_channels; stride=2, pad=down_padding))
-    end
+    downsamplers = isnothing(add_downsample) ? nothing : Chain(
+        Conv((3, 3), in_channels => out_channels; stride=2, pad=down_padding))
+
     CrossAttnDownBlock2D(resnets, attentions, downsamplers)
 end
 
