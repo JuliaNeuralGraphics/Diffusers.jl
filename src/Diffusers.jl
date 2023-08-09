a8f163f80cae02024afb9918391b58913bf7378f
Anton Smirnov <tonysmn97@gmail.com>
2023-08-10 00:12:30 +0300

    Do not do type piracy

--- src/Diffusers.jl
+++ src/Diffusers.jl
@@ -21,49 +21,6 @@ const Maybe{T} = Union{Nothing, T}
 get_pb(n, desc::String) = Progress(
     n; desc, dt=1, barglyphs=BarGlyphs("[=> ]"), barlen=50, color=:white)
 
-# TODO
-# This matches what PyTorch is doing.
-# Upstream to Flux.
-# Currently it is doing: (x - μ) / (σ + ϵ)
-# But instead it should: (x - μ) / sqrt(σ² + ϵ)
-function (ln::LayerNorm)(x::AbstractArray)
-    ϵ = convert(float(eltype(x)), ln.ϵ)
-    μ, σ² = _normalize(x; dims=1:length(ln.size))
-    y = ln.diag((x .- μ) .* inv.(sqrt.(σ² .+ ϵ)))
-    return y
-end
-
-function (gn::Flux.GroupNorm)(x::AbstractArray)
-    sz = size(x)
-    x2 = reshape(x, sz[1:end - 2]..., sz[end - 1] ÷ gn.G, gn.G, sz[end])
-    N = ndims(x2) # == ndims(x)+1
-    reduce_dims = 1:(N - 2)
-    affine_shape = ntuple(i -> i ∈ (N - 1, N - 2) ? size(x2, i) : 1, N)
-
-    μ, σ² = _normalize(x2; dims=reduce_dims)
-    γ = reshape(gn.γ, affine_shape)
-    β = reshape(gn.β, affine_shape)
-
-    ϵ = convert(float(eltype(x)), gn.ϵ)
-    scale = γ .* inv.(sqrt.(σ² .+ ϵ))
-    bias = -scale .* μ .+ β
-
-    return reshape(gn.λ.(scale .* x2 .+ bias), sz)
-end
-
-function _normalize(x::AbstractArray{Float16}; dims)
-    x_fp32 = Float32.(x)
-    μ, σ² = _normalize(x_fp32; dims)
-    m, v = Float16.(μ), Float16.(σ²)
-    return m, v
-end
-
-function _normalize(x; dims)
-    μ = mean(x; dims)
-    σ² = var(x; dims, mean=μ, corrected=false)
-    μ, σ²
-end
-
 include("timestep.jl")
 include("feed_forward.jl")
 include("attention.jl")
@@ -114,10 +71,8 @@ function main_clip()
     sd = StableDiffusion("runwayml/stable-diffusion-v1-5") |> f16 |> gpu
     println("Running StableDiffusion on $(get_backend(sd))")
 
-    prompts = [
-        "ancient house", "old house", "modern house",
-        "futuristic house", "cyberpunk house"]
-    clip(sd, prompts; n_inference_steps=10, n_interpolation_steps=24)
+    prompts = ["ancient house", "modern house"]
+    clip(sd, prompts; n_inference_steps=10, n_interpolation_steps=120)
     return
 end
 
